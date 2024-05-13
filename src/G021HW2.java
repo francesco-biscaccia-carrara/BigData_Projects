237c224af451783c52a0130cbca6074f84be2dd6
ferrarif1w <95565152+ferrarif1w@users.noreply.github.com>
2024-05-13 20:19:14 +0200

    ApproxOutliers commented, new table

--- src/G021HW2.java
+++ src/G021HW2.java
@@ -88,10 +88,19 @@ class MethodsHW2{
 
     public static void MRApproxOutliers(JavaRDD<Tuple2<Float, Float>> points, float D, int M) {
 
+         /*  Step A
+            - Map phase: (x,y) (coordinates of point) -> emit ( (i,j), 1 ) (key: identifier of cell)
+            - Reduce phase: for each cell (i,j), L_ij = { values of pairs with key (i,j) } = {1,1,...} ->
+            emit ( (i,j), |L_ij| ); |L_ij| = number of points in cell (i,j)
+         */
         JavaPairRDD<Tuple2<Integer, Integer>, Long> cellCount = points.mapToPair(
                 (pair) -> new Tuple2<>(determineCell(pair, D), 1L)
         ).reduceByKey(Long::sum);
 
+        /*  Step B
+            Collect the RDD as a Map and use a sequential algorithm to process the N3 and N7 values of each
+            cell.
+         */
         Map<Tuple2<Integer, Integer>, Long> tmpMap = cellCount.collectAsMap();
         HashMap<Tuple2<Integer, Integer>, Tuple3<Long, Long, Long>> pairSizeN3N7 = new HashMap<>();
 
@@ -99,14 +108,16 @@ class MethodsHW2{
             Tuple2<Tuple2<Integer,Integer>,Long> pair =  new Tuple2<>(e.getKey(),e.getValue());
 
             pairSizeN3N7.put(pair._1, new Tuple3<>(pair._2, 0L, 0L));
-
+            //Notice: this for count itself too
             for (int i = -3; i < 4; i++) {
                 for (int j = -3; j < 4; j++) {
                     if (tmpMap.get(new Tuple2<>(pair._1._1 + i, pair._1._2 + j)) != null) {
                         long cellIJCount = tmpMap.get(new Tuple2<>(pair._1._1 + i, pair._1._2 + j));
                         if ((i < -1 || i > 1) || (j < -1 || j > 1))
+                            //In region C7
                             pairSizeN3N7.put(pair._1, new Tuple3<>(pair._2, pairSizeN3N7.get(pair._1)._2(), pairSizeN3N7.get(pair._1)._3() + cellIJCount));
                         else
+                            //In region C3
                             pairSizeN3N7.put(pair._1, new Tuple3<>(pair._2, pairSizeN3N7.get(pair._1)._2() + cellIJCount, pairSizeN3N7.get(pair._1)._3() + cellIJCount));
                     }
                 }
